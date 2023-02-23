00f515a2475943bef4bb15dfcef21795d1da7e63
Alessandro Capitolo <capilander88@gmail.com>
2023-02-23 10:21:11 +0100

    Enable compress on server side

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -111,7 +111,7 @@ export default async () => {
 		components: true,
 
 		// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-		buildModules: ['@nuxtjs/prismic', '~/modules/sitemapGenerator.ts'],
+		buildModules: ['@nuxtjs/prismic', '~/modules/sitemapGenerator.ts', 'nuxt-compress'],
 
 		// Modules: https://go.nuxtjs.dev/config-modules
 		modules: [
