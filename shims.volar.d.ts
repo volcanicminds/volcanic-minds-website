836452688a2a35bf4c04b7cf03ce56b5198eee19
Alessandro Capitolo <capilander88@gmail.com>
2023-03-15 14:51:00 +0100

    Working on partners slices

--- shims.volar.d.ts
+++ shims.volar.d.ts
@@ -1,6 +1,8 @@
 import Breadcrumbs from './components/Breadcrumbs.vue'
 import Footer from './components/Footer.vue'
 import Header from './components/Header.vue'
+import Loading from './components/Loading.vue'
+import RainbowBar from './components/RainbowBar.vue'
 import Sidebar from './components/Sidebar.vue'
 import WrapperCard from './components/wrapper/Card.vue'
 import WrapperContainer from './components/wrapper/Container.vue'
@@ -12,6 +14,8 @@ declare module 'vue' {
 		Breadcrumbs: typeof Breadcrumbs
 		Footer: typeof Footer
 		Header: typeof Header
+		Loading: typeof Loading
+		RainbowBar: typeof RainbowBar
 		Sidebar: typeof Sidebar
 		WrapperCard: typeof WrapperCard
 		WrapperContainer: typeof WrapperContainer
--- slices/Partners/index.vue
+++ slices/Partners/index.vue
@@ -1,12 +1,13 @@
 <template>
 	<WrapperSlice :margin-top="slice.primary.margin_top || false" :margin-bottom="slice.primary.margin_bottom || false">
-		<WrapperContainer>
-			<div class="left title">
-				<p class="h1 m0">{{ slice.primary.title }}</p>
+		<WrapperContainer class="grid-container">
+			<div class="title">
+				<p class="h1 mt0 mb2">{{ slice.primary.title }}</p>
+				<RainbowBar />
 			</div>
 			<div class="partners-container">
 				<div v-for="(item, i) in slice.items" :key="`slice-item-${i}`" class="flex items-center">
-					<WrapperPrismicImage :field="item.logo" :size="30" resize-by-height class="mr2" />
+					<WrapperPrismicImage :field="item.logo" :size="40" resize-by-height class="mr2" />
 					<span v-if="item.name" class="h3 font-regular">{{ item.name }}</span>
 				</div>
 			</div>
@@ -30,11 +31,15 @@ defineProps({
 </script>
 
 <style lang="stylus" scoped>
+.grid-container
+	display grid
+	grid-gap 50px
+	grid-template-columns 25% auto
 .title
 	max-width 300px
 
 .partners-container
 	display grid
-	grid-template-columns repeat(auto-fit, minmax(200px, 1fr))
-	grid-gap 15px
+	grid-template-columns repeat(auto-fit, minmax(170px, 1fr))
+	grid-gap 25px
 </style>
