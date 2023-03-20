4c7420b3646008b1b1d3f99d41fec1522c0820f1
Alessandro Capitolo <capilander88@gmail.com>
2023-03-20 15:42:15 +0100

    Restore closeSidebar on resolution change

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,6 +28,12 @@ export default Vue.extend({
 		window.removeEventListener('resize', this.onResize)
 	},
 	methods: {
+		closeSidebar() {
+			this.$store.commit('prismic/setIsSidebarOpened', false)
+			if (process.client) {
+				document.body.style.overflow = 'visible'
+			}
+		},
 		onResize() {
 			window.innerWidth > 831 && this.closeSidebar()
 		}
