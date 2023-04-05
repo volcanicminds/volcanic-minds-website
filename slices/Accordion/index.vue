647cf10fe26d9dcfb3089809e3570d59f81277c3
Alessandro Capitolo <capilander88@gmail.com>
2023-04-05 17:29:21 +0200

    Fix accessibility issues

--- slices/Accordion/index.vue
+++ slices/Accordion/index.vue
@@ -6,7 +6,11 @@
 	>
 		<WrapperContainer>
 			<div v-for="(item, i) in slice.items" :key="`accordion-${i}`" class="my2">
-				<div class="cursor-pointer flex items-center justify-between" @click="openAccordion(`accordion-${i}`)">
+				<div
+					class="cursor-pointer flex items-center justify-between"
+					role="button"
+					@click="openAccordion(`accordion-${i}`)"
+				>
 					<p class="m0 h2">{{ item.title }}</p>
 					<div class="accordion-toggle items-center justify-center flex flex-none">
 						<font-awesome-icon :icon="['fas', isOpened === `accordion-${i}` ? 'minus' : 'plus']" />
